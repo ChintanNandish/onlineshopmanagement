584e3a555b958fedcab2e5732681bf2f13bac049
Chintan Patel <chintan97patel@gmail.com>
2018-04-02 11:31:15 +0530

    Updated links and minor changes

--- templates-to-copy/template2/update_product_data_user.php
+++ templates-to-copy/template2/update_product_data_user.php
@@ -1,5 +1,34 @@
 <?php
 session_start();
+
+if (isset($_POST['firstname'])){
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $company = $_POST['company'];
+    $street = $_POST['street'];
+    $city = $_POST['city'];
+    $zip = $_POST['zip'];
+    $state = $_POST['state'];
+    $country = $_POST['country'];
+    $phone = $_POST['phone'];
+    $file_open = fopen('user_folder/user_data.json', 'a+');
+    $file_read_data = fread($file_open, filesize('user_folder/user_data.json'));
+    $file_data = json_decode($file_read_data, true);
+    $file_data['root'][$_SESSION['reg_email']]['firstname'] = $firstname;
+    $file_data['root'][$_SESSION['reg_email']]['lastname'] = $lastname;
+    $file_data['root'][$_SESSION['reg_email']]['contact_email'] = $email;
+    $file_data['root'][$_SESSION['reg_email']]['company'] = $company;
+    $file_data['root'][$_SESSION['reg_email']]['street'] = $street;
+    $file_data['root'][$_SESSION['reg_email']]['city'] = $city;
+    $file_data['root'][$_SESSION['reg_email']]['zip'] = $zip;
+    $file_data['root'][$_SESSION['reg_email']]['state'] = $state;
+    $file_data['root'][$_SESSION['reg_email']]['country'] = $country;
+    $file_data['root'][$_SESSION['reg_email']]['contact_phone'] = $phone;
+    file_put_contents('user_folder/user_data.json', json_encode($file_data));
+    fclose($file_open);
+}
+
 $data = $_SESSION['cart'];
 $file_name = 'product_data.json';
 $str = file_get_contents($file_name);
